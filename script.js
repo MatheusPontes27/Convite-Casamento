3adf6d5e1f9c4fc58284cceb81d3467a2c71928f
MatheusPontes27 <matheusemmanuel71@gmail.com>
2025-03-17 04:36:41 -0300

    update 5

--- script.js
+++ script.js
@@ -21,3 +21,39 @@ ScrollReveal().reveal('.container3', {
   duration: 1000,    // Tempo da animação (em milissegundos)
   easing: 'ease-in-out', // Efeito de transição
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  let audio = document.getElementById("background-music");
+
+  // Recupera o tempo salvo
+  let savedTime = localStorage.getItem("musicTime");
+
+  if (savedTime) {
+      audio.currentTime = parseFloat(savedTime);
+  }
+
+  // Salva o tempo da música antes de sair da página
+  window.addEventListener("beforeunload", function () {
+      localStorage.setItem("musicTime", audio.currentTime);
+  });
+
+  audio.play();
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  var audio = document.getElementById('background-music');
+  
+  // Verificar se o áudio foi encontrado
+  if (audio) {
+      // Tentando reproduzir o áudio automaticamente
+      audio.play().catch(function (error) {
+          console.log("Erro ao tentar reproduzir o áudio: ", error);
+      });
+  } else {
+      console.log("Elemento de áudio não encontrado");
+  }
+});
+
+
+
+
