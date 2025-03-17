3694e07da4eca4a7efe5277b8debd52d59854ae9
MatheusPontes27 <matheusemmanuel71@gmail.com>
2025-03-17 02:26:39 -0300

    update 3

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-function abrirNovaPagina() {
-    window.location.href = "pagina2.html"; // Redireciona para a nova página
-  }
-  
+ScrollReveal().reveal('.container', {
+  delay: 300,        // Atraso de 300ms antes da animação
+  distance: '50px',  // Distância do movimento (em pixels)
+  origin: 'bottom',  // O elemento aparece vindo de baixo
+  duration: 1000,    // Tempo da animação (em milissegundos)
+  easing: 'ease-in-out', // Efeito de transição
+});
