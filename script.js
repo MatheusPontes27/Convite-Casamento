a25ce002ea550159e2ac5242ad9905219ef91a3f
MatheusPontes27 <matheusemmanuel71@gmail.com>
2025-03-17 03:14:26 -0300

    update 4

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-ScrollReveal().reveal('.container', {
+ScrollReveal().reveal('.container1', {
+  delay: 300,        // Atraso de 300ms antes da animação
+  distance: '50px',  // Distância do movimento (em pixels)
+  origin: 'bottom',  // O elemento aparece vindo de baixo
+  duration: 1000,    // Tempo da animação (em milissegundos)
+  easing: 'ease-in-out', // Efeito de transição
+});
+
+ScrollReveal().reveal('.container2', {
+  delay: 300,        // Atraso de 300ms antes da animação
+  distance: '50px',  // Distância do movimento (em pixels)
+  origin: 'bottom',  // O elemento aparece vindo de baixo
+  duration: 1000,    // Tempo da animação (em milissegundos)
+  easing: 'ease-in-out', // Efeito de transição
+});
+
+ScrollReveal().reveal('.container3', {
   delay: 300,        // Atraso de 300ms antes da animação
   distance: '50px',  // Distância do movimento (em pixels)
   origin: 'bottom',  // O elemento aparece vindo de baixo
